6a1e813697d733485af11c2257ba766ed816032c
Maciej <maciekkra@gmail.com>
2021-01-10 18:43:48 +0100

    added PORT

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 
 
 @app.route('/')
@@ -7,4 +10,4 @@ def index():
     return "Hello world!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host='0.0.0.0',port=port)
